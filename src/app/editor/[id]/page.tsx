475d98f76a0430c3011ef7ecfca6235547e9185c
NitaCristianu <andrei.cn08@gmail.com>
2024-04-01 23:10:35 +0300

    remove db editor

--- src/app/editor/[id]/page.tsx
+++ src/app/editor/[id]/page.tsx
@@ -7,7 +7,6 @@ import Text from "./components/main/Text/Text";
 import Menu from "./components/main/Tabs/Menu";
 import GraphGallery from "./components/main/Tabs/GraphGallery";
 import Graphs from "./components/main/Geometry/Graphs";
-import prisma from "../../../../lib/prisma";
 import { useAtom } from "jotai";
 import { ePoints_Calc_data, ePoints_data } from "./data/elements";
 
@@ -16,9 +15,6 @@ interface props {
 }
 
 export default async function Home(Properties : props) {
-  const points = await prisma.point.findMany({where : {worldId : Properties.params.id}});
-  const points_calc = await prisma.point.findMany({where : {worldId : Properties.params.id}});
-  const segments = await prisma.segment.findMany({where : {worldId : Properties.params.id}});
 
   return (<>
     <Grid />
