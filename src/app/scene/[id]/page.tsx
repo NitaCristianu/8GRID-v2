3efa6542090fec855d96a8adaed62c5c72b22eb4
NitaCristianu <andrei.cn08@gmail.com>
2024-04-01 20:50:17 +0300

    database wo labels

--- src/app/scene/[id]/page.tsx
+++ src/app/scene/[id]/page.tsx
@@ -9,12 +9,12 @@ export default async function Home(Properties: { params: { id: string } }) {
   const points = await prisma.point.findMany();
   const points_calcs = await prisma.pointCalc.findMany({ where: { worldId: Properties.params.id } });
   const segments = await prisma.segment.findMany({ where: { worldId: Properties.params.id } });
-  const labels = await prisma.label.findMany({ where: { worldId: Properties.params.id } });
+  // const labels = await prisma.label.findMany({ where: { worldId: Properties.params.id } });
   return (<>
     <Grid />
     <Euclidian />
     <Text />
     <Graphs />
-    <Others points={points} points_calc={points_calcs} segments={segments} labels={labels} />
+    <Others points={points} points_calc={points_calcs} segments={segments} labels={[]} />
   </>)
 }
